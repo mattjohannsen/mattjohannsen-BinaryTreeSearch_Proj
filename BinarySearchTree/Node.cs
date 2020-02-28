76618f34b50782d27829112e40eed30e8467306e
matt johannsen <majohann@hotmail.com>
2020-02-28 14:29:31 -0600

    Updated Binary Tree class and Node class

--- BinarySearchTree/Node.cs
+++ BinarySearchTree/Node.cs
@@ -8,6 +8,13 @@ namespace BinarySearchTree
 {
     public class Node
     {
+        public int info;
+        public Node link;
 
+        public Node(int info)
+        {
+            this.info = info;
+            link = null;
+        }
     }
 }
