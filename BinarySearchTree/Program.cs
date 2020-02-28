a1440610fb68f3e36da137c0be13bf2e2478e6da
matt johannsen <majohann@hotmail.com>
2020-02-28 14:27:05 -0600

    Initial upload for Binary Search Tree Project

---
+++ BinarySearchTree/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("");
+            Console.WriteLine("This the the Binary Search Tree");
+            BinaryTree binarytree = new BinaryTree();
+            binarytree.CreateTree();
+            Console.ReadLine();
+        }
+    }
+}
