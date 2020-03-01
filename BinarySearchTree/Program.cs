8792fb89a9356cf73772e74de46169e881f0d8cf
matt johannsen <majohann@hotmail.com>
2020-03-01 12:37:30 -0600

    Successful addition of left node to a left node - Insert method|Node class

--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -14,7 +14,7 @@ namespace BinarySearchTree
             binarytree.Insert(100);
             binarytree.Insert(82);
             binarytree.Insert(116);
-            //binarytree.Insert(78);
+            binarytree.Insert(78);
             //binarytree.Insert(93);
             //binarytree.Insert(105);
             //binarytree.Insert(123);
