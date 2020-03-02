a587d03a32e4b4b63572597036f0d49a8dc03cec
matt johannsen <majohann@hotmail.com>
2020-03-02 10:16:01 -0600

    Tested all greater than/less than variables of leaf nodes, and program worked correctly.

--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -19,6 +19,7 @@ namespace BinarySearchTree
             binarytree.Insert(105);
             binarytree.Insert(123);
             //binarytree.Search(binarytree.root, 93);
+            Console.WriteLine("These values should all be true:");
             Console.WriteLine($"{binarytree.Search(binarytree.root, 105)}");
             Console.WriteLine($"{binarytree.Search(binarytree.root, 100)}");
             Console.WriteLine($"{binarytree.Search(binarytree.root, 82)}");
@@ -27,7 +28,15 @@ namespace BinarySearchTree
             Console.WriteLine($"{binarytree.Search(binarytree.root, 93)}");
             Console.WriteLine($"{binarytree.Search(binarytree.root, 105)}");
             Console.WriteLine($"{binarytree.Search(binarytree.root, 123)}");
+            Console.WriteLine("These values should all be false:");
             Console.WriteLine($"{binarytree.Search(binarytree.root, 77)}");
+            Console.WriteLine($"{binarytree.Search(binarytree.root, 79)}");
+            Console.WriteLine($"{binarytree.Search(binarytree.root, 85)}");
+            Console.WriteLine($"{binarytree.Search(binarytree.root, 95)}");
+            Console.WriteLine($"{binarytree.Search(binarytree.root, 102)}");
+            Console.WriteLine($"{binarytree.Search(binarytree.root, 110)}");
+            Console.WriteLine($"{binarytree.Search(binarytree.root, 117)}");
+            Console.WriteLine($"{binarytree.Search(binarytree.root, 125)}");
             Console.ReadLine();
         }
     }
