6da7fbcb68c6dbf53d2636a50a95e82ce0b2d522
matt johannsen <majohann@hotmail.com>
2020-03-02 07:58:31 -0600

    Changing method to only exist in BinaryTree class. - Successful creation of root node with left and right subnode.

--- BinarySearchTree/BinaryTree.cs
+++ BinarySearchTree/BinaryTree.cs
@@ -15,15 +15,51 @@ namespace BinarySearchTree
         {
             root = null;
         }
-        public void Insert(int data)
+        public void Insert(int inputData)
         {
             if (root != null)
+            //The root is not empty, so let's figure out where to put the new node
             {
-                root.Insert(data);
+                //Below is old code for Insert method on BinaryTree
+                //root.Insert(data);
+                //Above is the old code for Insert method on BinaryTree
+                //Below is new code for Insert method on BinaryTree
+                //If the inputData is greater than the current data, then go right.
+                if (inputData > root.data)
+                {
+                    //If this node's right node is empty, then create a right node for this node.
+                    if (root.right == null)
+                    {
+                        root.right = new Node(inputData);
+                    }
+                    else
+                    {
+                        //If this node's right node is not empty, then insert the inputData into this right node to continue.
+                        //This process will continue until we find an empty place to create a node for our value.
+                        root.right.Insert(inputData);
+                    }
+                }
+                else
+                //The inputData must be smaller than the current Node's data, so we will go left to search for a spot to create our new node.
+                {
+                    //If the inputData is less than or equal to the data, then insert into the left node.
+                    if (root.left == null)
+                    {
+                        //If the leftnode is empty, then create a left node for this node.
+                        root.left = new Node(inputData);
+                    }
+                    else
+                    {
+                        //If this node's left node is not empty, then insert the inputData into this left node to continue.
+                        //This process will continue until we find an empty place to create a node for our value.
+                        root.left.Insert(inputData);
+                    }
+                }
             }
             else
+            //The root is empty, so make a new root node
             {
-                root = new Node(data);
+                root = new Node(inputData);
             }
         }
         public bool Search(int inputData)
--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -14,20 +14,20 @@ namespace BinarySearchTree
             binarytree.Insert(100);
             binarytree.Insert(82);
             binarytree.Insert(116);
-            binarytree.Insert(78);
-            binarytree.Insert(93);
-            binarytree.Insert(105);
-            binarytree.Insert(123);
+            //binarytree.Insert(78);
+            //binarytree.Insert(93);
+            //binarytree.Insert(105);
+            //binarytree.Insert(123);
             //binarytree.Search(93);
             //Console.WriteLine($"{binarytree.Search(105)}");
-            Console.WriteLine($"{binarytree.Search(100)}");
-            Console.WriteLine($"{binarytree.Search(82)}");
-            Console.WriteLine($"{binarytree.Search(116)}");
-            Console.WriteLine($"{binarytree.Search(78)}");
-            Console.WriteLine($"{binarytree.Search(93)}");
-            Console.WriteLine($"{binarytree.Search(105)}");
-            Console.WriteLine($"{binarytree.Search(123)}");
-            Console.WriteLine($"{binarytree.Search(77)}");
+            //Console.WriteLine($"{binarytree.Search(100)}");
+            //Console.WriteLine($"{binarytree.Search(82)}");
+            //Console.WriteLine($"{binarytree.Search(116)}");
+            //Console.WriteLine($"{binarytree.Search(78)}");
+            //Console.WriteLine($"{binarytree.Search(93)}");
+            //Console.WriteLine($"{binarytree.Search(105)}");
+            //Console.WriteLine($"{binarytree.Search(123)}");
+            //Console.WriteLine($"{binarytree.Search(77)}");
             Console.ReadLine();
         }
     }
